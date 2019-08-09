8d352864971fc315dfa8d32b41290aa64c350186
bitworxtech <53950861+bitworxtech@users.noreply.github.com>
2019-08-09 14:17:54 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #Be sure to edit out "enter site key here" and replace it with your site key surrounded by quotes
+#This should install silently and not display the menu bar icon but that doesn't seem to function properly 100% of the time
 
 echo "enter site key here" > dns_agent_site_key && sudo installer -dumplog -store -pkg "DNS Agent-Installer.pkg" -target /
